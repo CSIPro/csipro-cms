12a79c626cff9d0c275095f3b6175cb1d182377e
Luis Hernandez <luisernestohernandez1@hotmail.com>
2024-02-23 19:28:52 -0700

    Fix lint

--- src/collections/Miembros/index.ts
+++ src/collections/Miembros/index.ts
@@ -1,57 +1,52 @@
-import { CollectionConfig } from 'payload/types'
+import { CollectionConfig } from "payload/types";
 
 export const Miembros: CollectionConfig = {
-  slug: 'miembros',
+  slug: "miembros",
   admin: {
     useAsTitle: "nombres",
   },
   fields: [
     {
-      name: 'nombres',
-      label: 'Nombres',
-      type: 'text',
+      name: "nombres",
+      label: "Nombres",
+      type: "text",
       required: true,
     },
     {
-      name: 'apellidos',
-      label: 'Apellidos',
-      type: 'text',
+      name: "apellidos",
+      label: "Apellidos",
+      type: "text",
       required: true,
-      
     },
     {
-        name: 'email',
-        label: 'Email',
-        type: 'email',
-        required: true,
+      name: "email",
+      label: "Email",
+      type: "email",
+      required: true,
     },
-    
-    {
-        name: 'redes', // required
-        type: 'array', // required
-        label: 'Redes Sociales',
-        labels: {
-            singular: 'Red Social',
-            plural: 'Redes Sociales',
-        },
-        fields: [
-            // required
-            {
-                name: 'nombre',
-                label: 'Nombre de Red Social',
-                type: 'text',
-            },
-            
-            {
-                name: 'link',
-                label: 'Link a Red Social',
-                type: 'text',
-            },
-            
-        ],
-    },
-    
-    
-  ],
-}
 
+    {
+      name: "redes", // required
+      type: "array", // required
+      label: "Redes Sociales",
+      labels: {
+        singular: "Red Social",
+        plural: "Redes Sociales",
+      },
+      fields: [
+        // required
+        {
+          name: "nombre",
+          label: "Nombre de Red Social",
+          type: "text",
+        },
+
+        {
+          name: "link",
+          label: "Link a Red Social",
+          type: "text",
+        },
+      ],
+    },
+  ],
+};
